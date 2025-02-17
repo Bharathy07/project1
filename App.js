87fa41268d885ea601baf24c7fbb706f627b3b51
Bharathy07 <23csea07bharathy@gmail.com>
2025-02-17 22:11:24 +0530

    code2

--- App.js
+++ App.js
@@ -1,21 +1,12 @@
 import React from "react";
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-
 import Quiz from "./components/Quiz";
-import "./App.css";  // Your app styling
 
 const App = () => {
     return (
-        <Router>
-            <div>
-                <Routes> 
-                    {/* Quiz Page will be shown on the /quiz route */}
-                    <Route path="/quiz" element={<Quiz />} />
-                    
-                    
-                </Routes>
-            </div>
-        </Router>
+        <div>
+            
+            <Quiz />
+        </div>
     );
 };
 
---
+++ QuizPage.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const QuizPage = () => {
+    return (
+        <div>
+            <h1>Quiz Page</h1>
+            <p>This is where the flashcard quiz will be displayed.</p>
+        </div>
+    );
+};
+
+export default QuizPage;
